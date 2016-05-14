7099235aaac105ee68de7d404299840d1fbee7a3
Aaron Franks <aaron.franks@gmail.com>
2016-05-13 22:35:26 -0700

    Add preliminary eslint file

--- devserver.js
+++ devserver.js
@@ -1,13 +1,13 @@
-var app = require('express')()
-var webpack = require('webpack')
-var webpackConfig = require('./webpack.config')
-var compiler = webpack(webpackConfig)
- 
-app.use(require("webpack-dev-middleware")(compiler, {
+const app = require('express')()
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config')
+const compiler = webpack(webpackConfig)
+
+app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath
 }))
-app.use(require("webpack-hot-middleware")(compiler))
+app.use(require('webpack-hot-middleware')(compiler))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname })
